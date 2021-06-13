d358f85e75b76383e354dbf25dd3856b3b89f1a1
Federico Lenarduzzi <flenarduzzi@simtlix.com>
2021-06-13 17:48:46 -0300

    Parallelize API calls to optimize time.

--- server.js
+++ server.js
@@ -12,34 +12,44 @@ const app = express();
 app.use(bodyParser.json());
 
 
-async function getRates() {
-    const response = await axios.get(config.rates_conversion_url + config.api_key);
-    return response.data.rates;
+function getRates() {
+    return axios.get(config.rates_conversion_url + config.api_key)
+        .then((response) => {
+            return response.data.rates;
+        });
 }
 
-async function prepareResponse(data) {
+function getTraceInfo(req) {
+    const traceIp = req.body.ip.toString();
+    const trace_url = config.ip_trace_url.replace("YOUR_ADDRESS", traceIp);
 
-    // Calculate conversion rate
-    let rates = await getRates();
+    return axios.get(trace_url)
+        .then((response) => {
+            return response.data
+        });
+}
 
+function prepareResponse(data, rates) {
+    console.log(1, data);
+    console.log(2, rates);
     const fromPrice = rates[config.base_currency];
     const toPrice = rates[data.currency];   // to = data.countryCode
     const conversionRate = toPrice / fromPrice;
 
     return {
-        "ip":data.query,
+        "ip": data.query,
         "name": data.country,
         "code": data.countryCode,
         "lat": data.lat,
         "lon": data.lon,
         "currencies": [
             {
-                "iso":data.currency,
-                "symbol":  currencySymbols[data.currency],
+                "iso": data.currency,
+                "symbol": currencySymbols[data.currency],
                 "conversion_rate": conversionRate
             }, {
                 "iso": config.base_currency,
-                "symbol":  currencySymbols[config.base_currency],
+                "symbol": currencySymbols[config.base_currency],
                 "conversion_rate": 1
             }
         ],
@@ -49,15 +59,12 @@ async function prepareResponse(data) {
 
 // App Routes
 app.post('/traces', async (req, res) => {
-    const trace_url = config.ip_trace_url.replace("YOUR_ADDRESS", req.body.ip);
-    axios.get(trace_url)
-        .then(async response => {
-            const result = await prepareResponse(response.data);
-            res.send(result);
-        })
-        .catch(error => {
-            console.log(error);
-        });
+    var p1 = getTraceInfo(req);
+    var p2 = getRates();
+    Promise.all([p1, p2]).then((values) => {
+        const result = prepareResponse(values[0], values[1]);
+        res.send(result);
+    });
 });
 
 app.listen(config.app_port, config.app_host);
